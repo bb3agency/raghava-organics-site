d758d693b79d9559e56a8104c1676f4bfc76afc9
umesh-404 <umeshbtech05@gmail.com>
2026-06-02 23:38:32 +0530

    Implemented admin dashboard and UI

--- run-backend-e2e.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d D:\Agency\Clients\raghava-organics\raghava-organics-site\backend
-npm run test:e2e
--- run-frontend-integration-tests.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d D:\Agency\Clients\raghava-organics\raghava-organics-site\frontend
-npm run test:integration
